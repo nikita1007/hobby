4d54e4dcbb9052aafbd2ee8e04f8a408be1fdd29
nikita1007 <n_mancurov@mail.ru>
2024-11-06 18:02:04 +0500

    feat: Добавлен файл локализации русского языка для таблиц ресурсов

---
+++ resources/lang/ru/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'filament' => [
+        'table' => [
+            'no_records' => 'Нет записей для :resource',
+            'no_search_results' => 'По вашему запросу записей не найдено'
+        ],
+    ],
+];
